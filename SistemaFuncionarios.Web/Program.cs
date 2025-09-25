cb9a78398834217a531a1a11e0d1b11807f56ecd
Isadora Peixoto <isadorapgalego@gmail.com>
2025-09-25 16:33:59 -0300

    cria configurações com o banco

--- SistemaFuncionarios.Web/Program.cs
+++ SistemaFuncionarios.Web/Program.cs
@@ -2,8 +2,14 @@ using Microsoft.EntityFrameworkCore;
 using SistemasFuncionarios.Data.Context;
 using SistemasFuncionarios.Data.Repositories;
 using SistemasFuncionarios.Domain.Interfaces;
+using DotNetEnv;
+
+Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = $"Host={Environment.GetEnvironmentVariable("DB_HOST")};Database={Environment.GetEnvironmentVariable("DB_NAME")};Username={Environment.GetEnvironmentVariable("DB_USER")};Password={Environment.GetEnvironmentVariable("DB_PASSWORD")};Port={Environment.GetEnvironmentVariable("DB_PORT")}";
+
 
 builder.Services.AddRazorPages();
 
@@ -33,7 +39,7 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     using var scope = app.Services.CreateScope();
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
@@ -46,6 +52,6 @@ if (app.Environment.IsDevelopment())
         var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
         logger.LogError(ex, "Erro ao aplicar migrações do banco de dados");
     }
-}
+}*/
 
 app.Run();
